7f038647fadd8fc5c63b5a380672100120d9f946
Parabeac-Cloud <admin@parabeac.com>
2023-04-04 05:15:42 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/newparabeac/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newparabeac
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/newparabeac_theme.g.dart
@@ -0,0 +1,19 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:newparabeac/theme/newparabeac_colors.g.dart';
+
+class NewparabeacTheme {
+
+static const ColorScheme light = ColorScheme.light(onPrimary: NewparabeacColors.lightOnPrimary,primary: NewparabeacColors.lightPrimary,primaryContainer: NewparabeacColors.lightPrimaryContainer,secondary: NewparabeacColors.lightSecondary,onSecondary: NewparabeacColors.lightOnSecondary,onPrimaryContainer: NewparabeacColors.lightOnPrimaryContainer,secondaryContainer: NewparabeacColors.lightSecondaryContainer,onSecondaryContainer: NewparabeacColors.lightOnSecondaryContainer,shadow: NewparabeacColors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData( colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: NewparabeacColors.darkPrimary,onPrimary: NewparabeacColors.darkOnPrimary,primaryContainer: NewparabeacColors.darkPrimaryContainer,onPrimaryContainer: NewparabeacColors.darkOnPrimaryContainer,secondary: NewparabeacColors.darkSecondary,onSecondary: NewparabeacColors.darkOnSecondary,secondaryContainer: NewparabeacColors.darkSecondaryContainer,onSecondaryContainer: NewparabeacColors.darkOnSecondaryContainer,shadow: NewparabeacColors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData( colorScheme: dark,);
+}
